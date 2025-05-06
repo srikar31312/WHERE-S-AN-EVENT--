4dbe6c9f5ef4d473b15fa37e470dac32592fafc7
srikar31312 <vadlurisrikar@gmail.com>
2025-05-06 09:51:14 +0530

    ACP

---
+++ acp.py
@@ -0,0 +1,21 @@
+from tkinter import *
+def convert():
+    inches = float(entry.get())
+    cm = inches * 2.54
+    result.config(text=str(cm)+"cm")
+
+
+window =Tk()
+window.title("Inch to Cm converter")
+window.geometry("400x400")
+
+entry = Entry(window)
+entry.pack()
+
+button = Button(window, text= "convert to cm", command=convert)
+button.pack()
+
+result = Label(window,text="")
+result.pack()
+
+window.mainloop()
